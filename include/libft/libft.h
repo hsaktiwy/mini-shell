7cb99e7939d2dbb9d04a62b025299af51626d2ff
Abdelatif Igounad <aigounad@e1r2p2.1337.ma>
2023-05-12 11:39:10 +0100

    added ft_atoi to libft

--- include/libft/libft.h
+++ include/libft/libft.h
@@ -43,6 +43,7 @@ int		str_iswhitespaced(char *str);
 int		ft_strcmp(const char *s1, const char *s2);
 char	*ft_strchr(const char *s, int c);
 char	*str_join(char const *s1, char const *s2);
+int		ft_atoi(const char *str);
 
 // get next line 
 # ifndef BUFFER_SIZE
