d6957df72576c0b07317f6e8d475c4f17ebe50e9
Abdelatif Igounad <aigounad@e1r8p9.1337.ma>
2023-05-15 12:24:30 +0100

    NOTHING

--- src/execute.c
+++ src/execute.c
@@ -6,7 +6,7 @@
 /*   By: aigounad <aigounad@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 10:01:57 by aigounad          #+#    #+#             */
-/*   Updated: 2023/05/13 16:46:22 by aigounad         ###   ########.fr       */
+/*   Updated: 2023/05/15 12:23:24 by aigounad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,88 +20,79 @@ int	is_builtin(char *cmd)
 		!ft_strcmp(cmd, "export") ||
 		!ft_strcmp(cmd, "unset") ||
 		!ft_strcmp(cmd, "env") ||
-		!ft_strcmp(cmd, "cat") || ////////tmp
 		!ft_strcmp(cmd, "exit"))
 		return (1);
 	return (0);
 }
-/*****************************************************/
-// void	set_signal_handlers();
 
-// // void	child_handler()
-// // {
-// // 	exit(0);
-// // }
-
-// int ft_cat(t_cmd *command, t_env *env)
-// {
-// 	(void)command;
-// 	char *cmd[10] = {
-// 		"/bin/cat",
-// 		NULL,
-// 	};
-// 	int id = fork();
-// 	if (id == 0)
-// 	{
-// 		signal(SIGINT, SIG_DFL);
-// 		execve(cmd[0], cmd, env->env);
-	
-// 	}
-// 	else{
-// 		wait(0);
-// 	}
-// 	return (0);
-//}
-/*************************************************/
-
-void	exec_builtin(t_cmd *cmd, t_env *env)
+int	exec_builtin(t_cmd *cmd, t_env *env)
 {
 	if (!ft_strcmp(cmd->cmd, "echo"))
-		g_minishell.exit_status = ft_echo(cmd);
+		return (ft_echo(cmd));
 	if (!ft_strcmp(cmd->cmd, "cd"))
-		g_minishell.exit_status = ft_cd(cmd, env);
+		return (ft_cd(cmd, env));
 	if (!ft_strcmp(cmd->cmd, "pwd"))
-		g_minishell.exit_status = ft_pwd(cmd);
+		return (ft_pwd(cmd));
 	if (!ft_strcmp(cmd->cmd, "export"))
-		g_minishell.exit_status = ft_export(cmd, env);
+		return (ft_export(cmd, env));
 	if (!ft_strcmp(cmd->cmd, "env"))
-		g_minishell.exit_status = ft_env(cmd, env);
+		return (ft_env(cmd, env));
 	if (!ft_strcmp(cmd->cmd, "unset"))
-		g_minishell.exit_status = ft_unset(cmd, env);
-	//////////////////////////////////////
-	// if (!ft_strcmp(cmd->cmd, "cat"))	//tmp
-	// 	ft_cat(cmd, env);				//tmp
+		return (ft_unset(cmd, env));
 	if (!ft_strcmp(cmd->cmd, "exit"))
-		g_minishell.exit_status = ft_exit(cmd);
+		return (ft_exit(cmd));
+	return (0);
 }
 
-void	exec_builtin_fork(t_cmd *cmd, t_env *env)
-{
-	pid_t	pid;
+// void	exec_builtin_fork(t_cmd *cmd, t_env *env)
+// {
+// 	pid_t	pid;
 
-	pid = fork();
-	if (pid < 0)
-		ft_perror("fork");
-	if (pid == 0)
+// 	pid = fork();
+// 	if (pid < 0)
+// 		ft_perror("fork");
+// 	if (pid == 0)
+// 	{
+// 		if (!ft_strcmp(cmd->cmd, "echo"))
+// 			ft_echo(cmd);
+// 		if (!ft_strcmp(cmd->cmd, "cd"))
+// 			ft_cd(cmd, env);
+// 		if (!ft_strcmp(cmd->cmd, "pwd"))
+// 			ft_pwd(cmd);
+// 		if (!ft_strcmp(cmd->cmd, "export"))
+// 			ft_export(cmd, env);
+// 		if (!ft_strcmp(cmd->cmd, "env"))
+// 			ft_env(cmd, env);
+// 		if (!ft_strcmp(cmd->cmd, "unset"))
+// 			ft_unset(cmd, env);
+// 		if (!ft_strcmp(cmd->cmd, "exit"))
+// 			ft_exit(cmd);
+// 		exit(0);
+// 	}
+// 	else
+// 		wait(&pid);
+// }
+
+void	execute_cmd(t_list *cmd, t_env *env)
+{
+	// pid_t	pid;
+	// int		fd[2] = {-1, -1};
+	// old_fd = fd[];
+	(void)env;
+
+	if (is_builtin(((t_cmd *)(((t_token *)(cmd->content))->value))->cmd) && g_minishell.n_commands == 1)
 	{
-		if (!ft_strcmp(cmd->cmd, "echo"))
-			ft_echo(cmd);
-		if (!ft_strcmp(cmd->cmd, "cd"))
-			ft_cd(cmd, env);
-		if (!ft_strcmp(cmd->cmd, "pwd"))
-			ft_pwd(cmd);
-		if (!ft_strcmp(cmd->cmd, "export"))
-			ft_export(cmd, env);
-		if (!ft_strcmp(cmd->cmd, "env"))
-			ft_env(cmd, env);
-		if (!ft_strcmp(cmd->cmd, "unset"))
-			ft_unset(cmd, env);
-		if (!ft_strcmp(cmd->cmd, "exit"))
-			ft_exit(cmd);
-		exit(0);
+		g_minishell.exit_status = exec_builtin((t_cmd *)(((t_token *)(cmd->content))->value), env);	//exec in parrent
+		return ;
 	}
-	else
-		wait(&pid);
+	// pid = fork();
+	// if (pid < 0)
+	// 	perror("fork");
+	// if (pid == 0)
+	// {
+	// 	close(fd[0]);
+	// 	dup2(fd[1], );
+	// }
 }
 
 void	execute(t_list *list, t_env *env)
@@ -110,14 +101,9 @@ void	execute(t_list *list, t_env *env)
 
 	while (curr_cmd)
 	{
-		if (is_builtin(((t_cmd *)(((t_token *)(curr_cmd->content))->value))->cmd))
-		{
-			if (g_minishell.n_commands == 1)
-				exec_builtin((t_cmd *)(((t_token *)(curr_cmd->content))->value), env);	//exec in parrent
-			else
-				exec_builtin_fork((t_cmd *)(((t_token *)(curr_cmd->content))->value), env); //exec in child
-		}
+		execute_cmd(curr_cmd, env);
 		curr_cmd = curr_cmd->next;
 	}
+	
 	printf(">>> EXIT_STATUS = [%d]\n", g_minishell.exit_status);
 }
--- test.c
+++ test.c
@@ -1,14 +1,13 @@
 #include <unistd.h>
+#include <stdio.h>
 #include <sys/wait.h>
 
 int	main(int ac, char **av, char **env)
 {
-	int id = fork();
-	if (id == 0)
-	{
-		execve(cmd[0], cmd, env);
-	
-	}
-	wait(0);
+	// char **argv = {"echo", "-n", "hello"};
+
+	char **argv = av + 1;
+	execve("/bin/ls", argv, env);
+	perror("execve");
 	return (0);
 }
