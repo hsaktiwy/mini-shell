eaf9429ee58f0005ea3b52593a218f87c37eab55
root <root@abdelatif>
2023-05-25 12:58:26 +0100

    Just notes

--- testing.sh
+++ testing.sh
@@ -6,3 +6,16 @@ echo $"$"
 echo $'$'
 
 export USER= // overflow
+
+ << "end" cat
+
+ < $f'file' cat   | $f = Make
+ 
+
+ < f < Makefile cat
+
+ls<dadd
+
+ << end cat > 1 | << end cat > 2 | << end cat > 3
+
+echo $USER'R'$USER""''""''""'$USER'$USE // cuz u have the last null var after single quotes
