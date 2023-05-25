79fd0ab9fd484e1d7282eb65b27a272d7ab3aadb
Abdelatif Igounad <aigounad@e1r1p1.1337.ma>
2023-05-25 13:42:57 +0100

    testing.sh

--- testing.sh
+++ testing.sh
@@ -7,15 +7,35 @@ echo $'$'
 
 export USER= // overflow
 
- << "end" cat
+<< "end" cat
 
- < $f'file' cat   | $f = Make
+
+
+
+
+{
+	< f < Makefile cat
+	ls<dadd				##bash: f: No such file or directory
+
+
+	ls | < dadd		
+	< dadd | ls		##bash: dadd: No such file or directory and ls executed
+
+	should add another flag if there is an error so i don't execute the command.
+}
+
+{
+	export $f = Make
+
+	< $f'file' cat
+}
  
+{
+	<< end cat > 1 | << end cat > 2 | << end cat > 3
+	
+	should add a function to generate heredoc file names so that each heredoc writes to a different file
+}
 
- < f < Makefile cat
-
-ls<dadd
-
- << end cat > 1 | << end cat > 2 | << end cat > 3
-
-echo $USER'R'$USER""''""''""'$USER'$USE // cuz u have the last null var after single quotes
+{
+	echo $USER'R'$USER""''""''""'$USER'$USE // cuz u have the last null var after single quotes
+}
